958f08f615a40bb2d05c17cf1530133cab4d78bd
zotel1 <zotelsigel@gmail.com>
2025-04-04 23:21:31 -0300

    Arrancamos con los ejemplos de quickSort

---
+++ src/quick_sort/EjemploUno.java
@@ -0,0 +1,30 @@
+package quick_sort;
+
+public class EjemploUno {
+
+    // Partition function
+    static int partition(int[] arr,  int low, int high){
+
+        // Choose the pivot
+        int pivot = arr[high];
+
+        // Index of smaller element and indicates
+        // the right  position of pivot found so far
+        int i = low -1;
+
+        // Traverse arr[low..high] and move all smaller
+        // elements to the left side. Elements from low to
+        // i are smaller after every iteration
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        // Move pivot after smaller elements and
+        // return its position
+        swap(arre, i + 1, high);
+        return i + 1;
+    }
+}
