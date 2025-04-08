813ccc25b89cdd946f10b0218e0516294b66da4d
zotel1 <zotelsigel@gmail.com>
2025-04-08 10:36:12 -0300

    en este ejemplo podemos ver como tenemos nuestros elementos en el array antes y despues de la busqueda lineal Elements before Linear Search Transposition: 2 23 14 5 6 9 8 12                                                                                                               Elements after Linear Search Transposition: 2 14 23 5 6 9 8 12

--- src/busquedaLineal/EjemploCuatro.java
+++ src/busquedaLineal/EjemploCuatro.java
@@ -32,7 +32,37 @@ public class EjemploCuatro {
         int i;
         // Traverse the array
         for( i = 0; i < arr.length; i++){
-            // if key is found, then swap
+            // if key is found, then swap the element with it's previus index
+            if (key == arr.A[i]){
+                // If key is first element
+                if (i == 0)
+                    return i;
+                int temp = arr.A[i];
+                arr.A[i] = arr.A[i - 1];
+                arr.A[i - 1] = temp;
+                return i;
+            }
         }
+        return -1;
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        // Given array arr[]
+        int tempArr[] = {2, 23, 14, 5, 6, 9, 8, 12};
+        Array arr = new Array(tempArr, 10, 8);
+
+        System.out.printf("Elements before Linear" + " Search Transposition: ");
+
+        // function Call for displaying the array arr[]
+        Display(arr);
+
+        // Function call for transposition
+        LinearSearchTransposition(arr, 14);
+
+        System.out.printf("Elements after Linear" + " Search Transposition: " );
+
+        // Function Call for displaying the array
+        Display(arr);
     }
 }
