c7689c9041bfc1cb3f2b2fb48a5602a4ba418f3d
zotel1 <zotelsigel@gmail.com>
2025-04-15 23:21:51 -0300

    En este ejemplo se muestra cómo inicializar una matriz y recorrerla mediante un bucle for para imprimir cada elemento.

---
+++ src/matrices/EjemploDos.java
@@ -0,0 +1,17 @@
+package matrices;
+
+public class EjemploDos {
+
+    public static void main(String[] args) {
+
+        // initial array
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        // size of array
+        int n = arr.length;
+
+        // traversing array
+        for (int i = 0; i < n; i++)
+            System.out.println(arr[i] + " ");
+    }
+}
