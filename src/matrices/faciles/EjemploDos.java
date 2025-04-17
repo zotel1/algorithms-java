0e6a4c0e52df8f69eeba9906837db24c40d872a3
zotel1 <zotelsigel@gmail.com>
2025-04-16 23:59:33 -0300

    enfoque recursivo, la idea es similar al enfoque iterativo

---
+++ src/matrices/faciles/EjemploDos.java
@@ -0,0 +1,17 @@
+package matrices.faciles;
+
+import java.util.Arrays;
+
+public class EjemploDos {
+
+    static int largest(int[] arr) {
+
+        Arrays.sort(arr);
+        return arr[arr.length -1];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 234, 45, 90, 9808};
+        System.out.println(largest(arr));
+    }
+}
