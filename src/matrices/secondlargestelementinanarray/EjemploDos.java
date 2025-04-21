c32909bc510b0e8b0ae3f1e04745047d2c47e167
zotel1 <zotelsigel@gmail.com>
2025-04-20 23:47:08 -0300

    Busqueda de dos pasos, el enfoque de recorrer la matriz dps veces. En el primer recorrido, encuentre el elemento maximo. En el segundo recorrido, encuentra el elemento maximo ignorando el que encontramos en el primer recorrido

---
+++ src/matrices/secondlargestelementinanarray/EjemploDos.java
@@ -0,0 +1,32 @@
+package matrices.secondlargestelementinanarray;
+
+public class EjemploDos {
+
+    // function to find the second largest element in the array
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+
+        int largest = -1, secondLargest = -1;
+
+        // Finding the largest element
+        for (int i= 0; i < n; i++) {
+            if( arr[i] > largest)
+                largest = arr[i];
+        }
+
+        // Finfing the second largest element
+        for (int i = 0; i < n; i++) {
+
+            // Update second largest if the current element is greater than second largest and not equal to the largest
+            if ( arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println(getSecondLargest(arr));
+    }
+}
