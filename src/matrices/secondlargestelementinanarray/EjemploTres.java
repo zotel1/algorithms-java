585c6bad60efade9ad2e2c4d34b8712ab4c8d85f
zotel1 <zotelsigel@gmail.com>
2025-04-20 23:58:21 -0300

    Busqueda de un solo paso, la idea es realizar un seguimiento del elemento mas grande y del segundo mas grande mientras se atraviesa la matriz, inicialice largest y secondLargest con -1, ahora para cualquier indice i, si arr[i] > mas grande, actualice secondLargest con largest y largest con arr[i], de lo contrario si arr[i] < mas grande y arr[i] > segundoMasGrande, actualice secondLargest con arr[i]

---
+++ src/matrices/secondlargestelementinanarray/EjemploTres.java
@@ -0,0 +1,32 @@
+package matrices.secondlargestelementinanarray;
+
+public class EjemploTres {
+
+    // Function to find the second largest element in the array
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+
+        int largest = -1, secondLargest = -1;
+
+        // finding the second largest element
+        for (int i = 0; i < n; i++) {
+
+            // if arr[i] > largest, update second largest with largest and largest with arr[i]
+            if(arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+
+            // if arr[i] < largest and arr[i] > second largest, update second largest with arr[i]
+            else if ( arr[i] < largest && arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        System.out.println(getSecondLargest(arr));
+    }
+}
