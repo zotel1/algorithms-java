2c9f97e452c54b7cc7e6b9a02335781a4f1a2f5f
zotel1 <zotelsigel@gmail.com>
2025-04-21 23:47:13 -0300

    Solucion ingenua: tres travesias, encuentra el primer elemento mas grande haciendo un recorrido. Halla el segundo mas grande encontrando el elemento mas grande que no sea igual al elemento encontrado en el conjunto 1. Encuentra el tercero mas grande encontrando el mas grande que no sea igual al primero y al segundo

---
+++ src/matrices/largestThreeDistinctElementsInArray/EjemploUno.java
@@ -0,0 +1,45 @@
+package matrices.largestThreeDistinctElementsInArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EjemploUno {
+
+    // Function to return three largest elements
+    public static List<Integer> get3largest(int[] arr) {
+        int fst = Integer.MIN_VALUE, sec = Integer.MIN_VALUE, thd = Integer.MIN_VALUE;
+
+        for (int x : arr) {
+            // if current element is greader than fst
+            if (x > fst) {
+                thd = sec;
+                sec = fst;
+                fst = x;
+            }
+
+            // If x is between sec and thd
+            else if (x > thd && x != sec && x != fst) {
+                thd = x;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        if (fst == Integer.MIN_VALUE) return res;
+        res.add(fst);
+        if (sec == Integer.MIN_VALUE) return res;
+        res.add(sec);
+        if (thd == Integer.MIN_VALUE) return res;
+        res.add(thd);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 13, 1, 10, 34, 1};
+        List<Integer> res = get3largest(arr);
+        for (int x : res) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+}
