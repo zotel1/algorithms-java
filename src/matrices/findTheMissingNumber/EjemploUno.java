e2efef4eb3af04f6b4fc05f320bd95145e083d98
zotel1 <zotelsigel@gmail.com>
2025-04-22 23:42:47 -0300

    Este enfoque recorre en iteracion cada numero desde 1 hasta donde es el tamaño de la matriz +1 y comprueba si el numero esta presente en la matriz. Para cada numero, utiliza un bucle anidado para buscar en la matriz. Si no se encuentra un numero, se devuelve como el numero que falta

---
+++ src/matrices/findTheMissingNumber/EjemploUno.java
@@ -0,0 +1,31 @@
+package matrices.findTheMissingNumber;
+
+// Encuentra el numero que falta
+public class EjemploUno {
+
+    static int missingNum(int[] arr) {
+        int n = arr.length + 1;
+
+        // Iterate from 1 to n and check if the current number us present
+        for (int i = 1; i <= n; i++) {
+            boolean found = false;
+            for ( int j = 0; j < n - 1; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // if the current number is not present
+            if (!found)
+                return i;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 4, 5, 3, 7, 1};
+        System.out.println(missingNum(arr));
+    }
+}
