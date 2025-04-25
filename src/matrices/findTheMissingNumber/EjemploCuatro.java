a38de35f54036e55ef778d60738d6638e0a52a31
zotel1 <zotelsigel@gmail.com>
2025-04-24 22:52:15 -0300

    XOR de un número consigo mismo es 0, es decir, x ^ x = 0 y la matriz dada arr[] tiene números en el rango [1, n]. Esto significa que el resultado de XOR de los primeros n números naturales con el XOR de todos los elementos  de la matriz será el  número que falta. Para ello, calculamos XOR de los primeros n números naturales y XOR de todos los elementos de la matriz arr[], y luego nuestro resultado será el XOR de ambos valores resultantes.

---
+++ src/matrices/findTheMissingNumber/EjemploCuatro.java
@@ -0,0 +1,24 @@
+package matrices.findTheMissingNumber;
+
+public class EjemploCuatro {
+
+    public static int missingNum(int[] arr) {
+        int n = arr.length + 1;
+        int xor1 = 0, xor2 = 0;
+
+        // XOR all numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            xor1 ^= i;
+        }
+
+        // Missing number is the XOR of xor1 and xor2
+        return xor1 ^ xor2;
+        }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 4, 5, 3, 7, 1 };
+        int res = missingNum(arr);
+        System.out.println(res);
+
+    }
+}
