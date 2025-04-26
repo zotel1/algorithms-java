3383795f175f65200552defc97ce61ea42e980ae
zotel1 <zotelsigel@gmail.com>
2025-04-26 20:37:36 -0300

    Cree una matriz de frecuencia de tamaño n+1 inicializada con ceros (usamos n+1 ya que los números van de 1 a n). Recorra la matriz de entrada e incremente el recuento de frecuencia para cada elemento en su índice correspondiente en la matriz de frecuencias. Atraviesa el conjunto de frecuencias desde el índice 1 hasta n: Si algún índice tiene frecuencia 0, ese índice es el número que falta Si cualquier índice tiene frecuencia 2, ese índice es nuestro número repetido Devuelve los números repetidos y faltantes

---
+++ src/matrices/missingAndRepeatingInAnArray/EjemploUno.java
@@ -0,0 +1,49 @@
+package matrices.missingAndRepeatingInAnArray;
+
+import java.util.ArrayList;
+
+public class EjemploUno {
+    static ArrayList<Integer> findTwoElement(int[] arr) {
+        int n = arr.length;
+
+        // Creating frequency array of size n+1 with
+        // initial values as 0. Note that array
+        // values will go upto n, that is why we
+        // have taken the size as n+1
+        int[] freq = new int[n + 1];
+        int repeating = -1;
+        int missing = -1;
+
+        // Find the frequency of all elements.
+        for (int i = 0; i < n; i++) {
+            freq[arr[i]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+
+            // For missing element, frequency
+            // will be 0.
+            if (freq[i] == 0) {
+                missing = i;
+            }
+
+            // For repeating element, frequency
+            // will be 2.
+            else if (freq[i] == 2) {
+                repeating = i;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeating);
+        result.add(missing);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 3};
+        ArrayList<Integer> ans = findTwoElement(arr);
+
+        System.out.println(ans.get(0) + " " + ans.get(1));
+    }
+}
