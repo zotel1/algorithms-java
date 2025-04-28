d932ee86b91175539cc1cac902c1077f6af2abde
zotel1 <zotelsigel@gmail.com>
2025-04-27 23:56:50 -0300

    Traverse the array and for each element get its absolute value. Use this value-1 as an index and make the element at that index negative. If we find that the element at that index is already negative, we’ve found our repeating number. After the first traversal, iterate through the array again looking for any positive value When we find a positive value at index i, i+1 is our missing number. Return both the repeating and missing numbers

---
+++ src/matrices/missingAndRepeatingInAnArray/EjemploDos.java
@@ -0,0 +1,47 @@
+package matrices.missingAndRepeatingInAnArray;
+
+import java.util.ArrayList;
+
+public class EjemploDos {
+    static ArrayList<Integer> findTwoElement(int[] arr) {
+        int n = arr.length;
+
+        int repeating = -1;
+
+        for (int i = 0; i < n; i++) {
+            int val = Math.abs(arr[i]);
+
+            if (arr[val - 1] > 0) {
+                arr[val - 1] = -arr[val - 1];
+            }
+            else {
+
+                // Element is repeating.
+                repeating = val;
+            }
+        }
+
+        int missing = -1;
+
+        // Value at missing value index
+        // will be positive.
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                missing = i + 1;
+                break;
+            }
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(repeating);
+        result.add(missing);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 3};
+        ArrayList<Integer> ans = findTwoElement(arr);
+
+        System.out.println(ans.get(0) + " " + ans.get(1));
+    }
+}
