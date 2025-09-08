1a2db06783c137f84e50f578cf8a0bb267bf2182
zotel1 <zotelsigel@gmail.com>
2025-09-07 22:25:50 -0300

    en el tercer ejemplo pasamos el codigo en c++ para luego transcribirlo en java

---
+++ src/codigofacilito/Tabulacion.java
@@ -0,0 +1,28 @@
+package codigofacilito;
+
+/*
+* int nthFibonacci(int n) {
+* if (n <= 1)
+*     return n;
+*
+* vector<int> dp(n + 1);
+*
+* dp[0] = 0;
+* dp[1] = 1;
+*
+* for (int i = 2; i <= n; ++i){
+*
+* dp[i] = dp[i - 1] + dp[i -2];
+* }
+*
+* return dp[n];
+* }
+* int main(){
+* int n = 5;
+* int result = nthFibonacci(n);
+* count << result << endl;
+* return 0;
+* */
+
+public class Tabulacion {
+}
